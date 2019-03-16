ef9d348c69fcba4a272cb55564861a56fff3e667
Lauren Stephens <9128505+lstephens46@users.noreply.github.com>
2019-03-16 11:47:25 -0500

    complete methods

--- DayBetween/Program.cs
+++ DayBetween/Program.cs
@@ -7,10 +7,51 @@ namespace DaysBetween
         public static void Main()
         {
             // TODO:  Get two dates from the user.
+            Console.WriteLine("\tDays Between Two Dates\t");
+
+            string firstDateString = GetDate();
+            string secondDateString = GetDate();
+
 
             Console.ReadLine();
         }
 
+
+        //call this method twice to get the dates
+        public static string GetDate()
+        {
+            Console.Write("Please enter a date in MM/DD/YYYY format:\t");
+            string dateString = Console.ReadLine();
+            return dateString;
+        }
         // TODO: Create that has a method that counts the number of days between two different dates.
+
+        public static TimeSpan DaysBetween(string firstDate, string secondDate)
+        {   //convert date string to DateTime object
+
+            DateTime parsedFirstDate = DateTime.Parse(firstDate);
+            DateTime parsedSecondDate = DateTime.Parse(secondDate);
+
+            TimeSpan daysBetween;
+
+            if (parsedFirstDate > parsedSecondDate)
+            {
+                daysBetween = parsedFirstDate.Subtract(parsedSecondDate);
+                Console.WriteLine($"this is the if{daysBetween}");
+                return daysBetween;
+            }
+            else//(parsedSecondDate > parsedFirstDate)
+            {
+                daysBetween = parsedSecondDate.Subtract(parsedFirstDate);
+                Console.WriteLine($"this is the else {daysBetween}");
+                return daysBetween;
+            }
+            //else
+            //{   
+            //    Console.WriteLine("You did not enter a valid date");
+            //    return;
+            //}
+
+        }
     }
 }
